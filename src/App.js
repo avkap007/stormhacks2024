67ac9f02dd04de12104fb611c4786860360ba001
Max Zhang <maxzh916@gmail.com>
2024-05-19 07:49:57 -0700

    updated map to leaflet, locked starting position to user's current position

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import PostList from './components/PostList';
 import NewPost from './components/NewPost';
-import Maps from './components/map/Maps.js';
+import Maps from './components/Maps.js';
 import { PostProvider } from './components/PostContext';
 import { Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
@@ -33,10 +33,10 @@ function App() {
       <PostProvider>
         <Routes>
           <Route path="/" element={<Maps />} />
-          <Route path="/" element={<PostList />} />
-          <Route path="/new-post" element={<NewPost />} />
+          {/* <Route path="/" element={<PostList />} />
+          <Route path="/new-post" element={<NewPost />} /> */}
         </Routes>
-        <FloatingButton />
+        {/* <FloatingButton /> */}
       </PostProvider>
     </Router>
   );
