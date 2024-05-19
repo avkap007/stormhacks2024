425191f7a75818cb22796ea330672fdb68685c3d
mantajdhillon <mantajdhillon91@gmail.com>
2024-05-18 23:06:17 -0700

    created some stylings for new post

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -20,12 +20,16 @@ function NewPost() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <textarea
+    <form style={{justifyContent: "right"}}onSubmit={handleSubmit}>
+      <textarea style={{border: "3px solid #000", borderRadius: "60px", align: "left", width: "95%", padding: "30px", fontSize: "20px"}}
         value={content}
         onChange={(e) => setContent(e.target.value)}
       ></textarea>
-      <button type="submit">Post</button>
+      <select style={{alignSelf: "right", border: "1px solid #AAA",  borderRadius: "2px",  boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",  color: "#555",  fontSize: "inherit",  margin: "0",  overflow: "hidden",  paddingTop: "2px",  paddingBottom: "2px",  textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
+        <option value="someOption">Anonymous</option>
+        <option value="otherOption">Show Username</option>
+      </select>
+      <button style={{alignSelf: "right", backgroundColor: "#333", color: "#fff", border: "none", padding: "10px 20px", borderRadius: "4px", fontSize: "16px", cursor: "pointer"}} type="submit">Post</button>
     </form>
   );
 }
