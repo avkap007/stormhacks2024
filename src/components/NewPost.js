e1057e892da6ef67d3d1bced0e1641e5315cb850
mantajdhillon <mantajdhillon91@gmail.com>
2024-05-18 23:20:50 -0700

    snall changes

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -20,16 +20,20 @@ function NewPost() {
   };
 
   return (
-    <form style={{justifyContent: "right"}}onSubmit={handleSubmit}>
-      <textarea style={{border: "3px solid #000", borderRadius: "60px", align: "left", width: "95%", padding: "30px", fontSize: "20px"}}
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      ></textarea>
-      <select style={{alignSelf: "right", border: "1px solid #AAA",  borderRadius: "2px",  boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",  color: "#555",  fontSize: "inherit",  margin: "0",  overflow: "hidden",  paddingTop: "2px",  paddingBottom: "2px",  textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
-        <option value="someOption">Anonymous</option>
-        <option value="otherOption">Show Username</option>
-      </select>
-      <button style={{alignSelf: "right", backgroundColor: "#333", color: "#fff", border: "none", padding: "10px 20px", borderRadius: "4px", fontSize: "16px", cursor: "pointer"}} type="submit">Post</button>
+    <form onSubmit={handleSubmit}>
+      <div style={{justifyContent:"center"}}>
+        <textarea style={{border: "3px solid #000", borderRadius: "60px", align: "left", width: "75%", padding: "30px", fontSize: "20px", position: "relative"}}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+      </div>
+      <div style={{alignSelf: "right", position: "relative"}}>
+        <select style={{alignSelf: "right", border: "1px solid #AAA",  borderRadius: "2px",  boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",  color: "#555",  fontSize: "inherit",  margin: "0",  overflow: "hidden",  paddingTop: "2px",  paddingBottom: "2px",  textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
+          <option value="someOption">Anonymous</option>
+          <option value="otherOption">Show Username</option>
+        </select>
+        <button style={{alignSelf: "right", backgroundColor: "#333", color: "#fff", border: "none", padding: "10px 20px", borderRadius: "4px", fontSize: "16px", cursor: "pointer"}} type="submit">Post</button>
+      </div>
     </form>
   );
 }
